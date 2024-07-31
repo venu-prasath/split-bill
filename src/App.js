9288c9962b224419a22ea9df096d8f47c6252ab1
venu-prasath <venuprasth133@gmail.com>
2024-07-31 17:44:35 -0400

    adding key prop to fix rendering

--- src/App.js
+++ src/App.js
@@ -67,7 +67,11 @@ export default function App() {
       </div>
 
       {selectedFriend && (
-        <FormSplitBill friend={selectedFriend} onSplitBill={handleSplitBill} />
+        <FormSplitBill
+          key={selectedFriend.id}
+          friend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
       )}
     </div>
   );
